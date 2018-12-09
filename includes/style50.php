741ff1d79c19614a8010c8d1403a3fbd1453797f
Chris Dillon <chrisdillon.us@gmail.com>
2018-12-09 07:09:49 -0500

    Update CSS for WP 5

---
+++ includes/style50.php
@@ -0,0 +1,39 @@
+<!--suppress ALL -->
+<style>
+    /* Wider Admin Menu for WordPress <?php echo $wp_version; ?>  */
+
+    #wpcontent,
+    #wpfooter {
+        margin-left: <?php echo $wpx; ?>;
+    }
+
+    #adminmenuback,
+    #adminmenuwrap,
+    #adminmenu,
+    #adminmenu .wp-submenu {
+        width: <?php echo $wpx; ?>;
+    }
+
+    #adminmenu .wp-submenu {
+        left: <?php echo $wpx; ?>;
+    }
+
+    #adminmenu .wp-not-current-submenu .wp-submenu,
+    .folded #adminmenu .wp-has-current-submenu .wp-submenu {
+        min-width: <?php echo $wpx; ?>;
+    }
+
+    /* Query Monitor plugin */
+    body.wp-admin #qm {
+        margin-left: <?php echo $wpx; ?> !important;
+    }
+
+    /* Gutenberg */
+    .auto-fold .edit-post-header {
+        left: <?php echo $wpx; ?>;
+    }
+
+    .auto-fold .components-notice-list {
+        left: <?php echo $wpx; ?>;
+    }
+</style>
